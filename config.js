a01a916294398ff0ac241deb01960e917f8af085
Sathuta628 <janithsathsa.r.a.55.567@gmail.com>
2025-04-07 22:13:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -37,10 +37,10 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
   //  සෙසන් අයිඩී එක දාන්න👇👇👇 (උඩු කොමා දෙක ඇතුලේ)
-  SESSION_ID: process.env.SESSION_ID || "", 
+  SESSION_ID: process.env.SESSION_ID || "Golden-Queen-MD-cpJASbAY#PmTL-PweBS6FFnLid0VA60evP8-LL_Us65I47w88xWIftrc", 
 
 //  Mongodp URl එක දාන්න 👇👇👇 (උඩු කොමා දෙක ඇතුලේ )
-  MONGODB: process.env.MONGODB || "",
+  MONGODB: process.env.MONGODB || "mongodb+srv://c95629830:3D3MXwbTRTFkAPus@cluster0.5bgx4.mongodb.net/",
 
 
 MAX_SIZE: 500,
